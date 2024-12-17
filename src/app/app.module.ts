971a3a31a876ee401b3b6386736b4c14e709246a
samuel <211012394@edu.vut.ac.za>
2024-12-17 14:31:02 +0200

    update app.module.ts, contact.component.html and contact.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
@@ -29,8 +29,7 @@ import { SkillsComponent } from './components/skills/skills.component';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-
-    
+    ReactiveFormsModule
   ],
   providers: [ProjectService],
   bootstrap: [AppComponent]
--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,10 +8,42 @@ import { Router } from '@angular/router';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
-
-  constructor(private route: Router) { }
-
   ngOnInit() {
+  }
+
+  contactForm: FormGroup;
+
+  constructor(private route: Router, private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(5)]],
+      email: ['',[Validators.required, Validators.email]],
+      message: ['',[Validators.required, Validators.minLength(6)]]
+   });
+  }
+
+  
+  get name() {
+    return this.contactForm.get('name');
+  }
+
+  
+  public get email() {
+    return this.contactForm.get('email');
+  }
+
+  
+  public get message(){
+    return this.contactForm.get('message');
+  }
+  
+  onSubmit(){
+    if (this.contactForm.valid){
+      console.log('Form Submitted!', this.contactForm.value);
+      alert("Your message has been sent successfully!");
+      this.contactForm.reset();
+    }else {
+      alert("Please fill in all fields!");
+    }
   }
 
 }
