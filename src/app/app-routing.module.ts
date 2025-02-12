a091ec8a9875832b45ad4cc119b5ef1ffeb95ef4
samuel <211012394@edu.vut.ac.za>
2025-02-12 18:43:22 +0200

    another commit, few updates and code changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ import { SkillsComponent } from './components/skills/skills.component';
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'projects', component: ProjectsComponent},
-  {path:'About',component:BioComponent},
+  {path:'about',component:BioComponent},
   {path: 'contact', component: ContactComponent},
   {path: 'skills', component: SkillsComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full' }
