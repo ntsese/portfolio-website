5e68b9060ce9bad27bd88b39d6ab633efc9b5ce7
samuel <211012394@edu.vut.ac.za>
2025-05-18 22:20:15 +0200

    Add SVG icons for various frameworks and tools Modify the Skill page

--- src/app/interfaces/skills.ts
+++ src/app/interfaces/skills.ts
@@ -1,2 +1,24 @@
-export interface Skills {
-}
+export interface Skill {
+    name: string;
+    level: string;
+    icon: string;
+  }
+  
+  export const SKILLS: Skill[] = [
+    { name: 'Angular', level: 'Advanced', icon: 'assets/icons-svg/Angular-Light.svg' },
+    { name: 'JavaScript', level: 'Intermediate', icon: 'assets/icons-svg/JavaScript.svg' },
+    { name: 'TypeScript', level: 'Intermediate', icon: 'assets/icons-svg/TypeScript.svg' },
+    { name: 'HTML', level: 'Advanced', icon: 'assets/icons-svg/HTML.svg' },
+    { name: 'CSS', level: 'Advanced', icon: 'assets/icons-svg/CSS.svg' },
+    { name: 'Bootstrap', level: 'Intermediate', icon: 'assets/icons-svg/Bootstrap.svg' },
+    { name: 'SASS', level: 'Beginners', icon: 'assets/icons-svg/Sass.svg' },
+    { name: 'Node.js', level: 'Beginners', icon: 'assets/icons-svg/NodeJS-Light.svg' },
+    { name: 'SQL', level: 'Intermediate', icon: 'assets/icons-svg/SQLite.svg' },
+    { name: 'MySQL', level: 'Intermediate', icon: 'assets/icons-svg/MySQL-Light.svg' },
+    { name: 'Git', level: 'Intermediate', icon: 'assets/icons-svg/Git.svg' },
+    { name: 'GitHub', level: 'Intermediate', icon: 'assets/icons-svg/Github-Light.svg' },
+    { name: 'Azure', level: 'Intermediate', icon: 'assets/icons-svg/Azure-Light.svg' },
+    { name: 'C#', level: 'Intermediate', icon: 'assets/icons-svg/CS.svg' },
+    { name: 'ASP.NET', level: 'Intermediate', icon: 'assets/icons-svg/DotNet.svg' },
+    { name: 'VB.NET', level: 'Beginners', icon: 'assets/icons-svg/DotNet.svg' },
+  ];
